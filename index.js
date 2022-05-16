bd8cb84b2d7bbe6e145c1a9f2d48b8004f021b34
--replace-all <terrencejung928@gmail.com>
2022-05-16 15:01:33 -0700

    commit tree 02effccdb54569f1c1cbbe2fda1e5445c4a1d1be

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random());
+
+const num3 = 32;
+const num4 = 7;
+const mod = num3 % num4;
+
+const max = Math.max(1, 2, 10, 20);
